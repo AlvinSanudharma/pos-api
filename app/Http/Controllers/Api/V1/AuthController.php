f8f2af3c458cdccceb4c0390b19a2a6d600f4e97
AlvinSanudharma <alvinsanudharma@gmail.com>
2026-01-25 10:45:26 +0800

    Login API

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -2,11 +2,40 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthenticateUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function login(AuthenticateUserRequest $request)
+    {
+        $credentials = $request->validated();
+
+        $user = User::where('email', $credentials['email'])->first();
+
+        if (!$user || Hash::check($credentials['password'], $user->password)) {
+            return ApiResponse::error(
+                'Invalid credentials',
+                Response::HTTP_UNAUTHORIZED
+            );
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return ApiResponse::success(
+            [
+                'token' => $token,
+                'user' => $user
+            ],
+            'Login Successfull'
+        );
+    }
+
     public function me(Request $request)
     {
         return $request->user();
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('V1')->group(function() {
+Route::prefix('v1')->group(function() {
+   Route::post('/login', [AuthController::class, 'login']);
+
    Route::middleware('auth:sanctum')->group(function() {
         Route::get('/me', [AuthController::class, 'me']);
 
