4a93fa76f4be01b4219a164cd21033e2e96b53c0
AlvinSanudharma <alvinsanudharma@gmail.com>
2026-01-29 20:38:33 +0800

    Fix Customer Resource

--- app/Http/Resources/CustomerResource.php
+++ app/Http/Resources/CustomerResource.php
@@ -17,7 +17,7 @@ class CustomerResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'phone' => $this->phone,
+            'phone' => $this->whenHas('phone'),
         ];
     }
 }
